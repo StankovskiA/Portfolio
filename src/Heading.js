4fcff1e9ce83cf9ddd5c2f734bf57ddf0f2bf09d
StankovskiA <stankovski.a@gmail.com>
2021-02-24 13:45:11 +0100

    Git Button

--- src/Heading.js
+++ src/Heading.js
@@ -25,6 +25,14 @@ function clicked3()
   } else {
     x.style.display = "none";
   }
+}function clicked4()
+{
+  var x = document.getElementById('hPanela');
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
 }
 export default function Heading() {
     return (
@@ -44,10 +52,14 @@ export default function Heading() {
         </button>
         <button type="button" className="gButton"  onClick={clicked3}>
           <img alt="e"  src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202090instagramlogosocialsocialmedia-115598_115703.png"></img>
+        </button>
+        <button type="button" className="gButton"  onClick={clicked4}>
+          <img alt="e"  src="http://pngimg.com/uploads/github/github_PNG40.png"></img>
         </button> <br/>
         <span className="gPanel" id="gPanela">stankovski.a@gmail.com</span>
         <span className="gPanel" id="lPanela"><a target="_blank" rel="noreferrer" href="http://linkedin.com/in/stankovskialeksandar">My LinkedIn</a></span>
         <span className="gPanel" id="iPanela"><a target="_blank" rel="noreferrer" href="https://www.instagram.com/friendlycoder/">My Instragram</a></span>
+        <span className="gPanel" id="hPanela"><a target="_blank" rel="noreferrer" href="https://github.com/StankovskiA">My Github</a></span>
 
         </p>
         
